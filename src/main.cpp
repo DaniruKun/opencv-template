96cf0e72c39f0aedb66a4b33f39aa7d6223acf19
dpetrovs <dpetrovs@evolutiongaming.com>
2019-11-30 19:56:19 +0200

    Set up working CMAKE project

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "App.hpp"
+#include "opencv2/core.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!";
+    return 0;
+}
---
+++ src/main.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <mutex>
+#include <atomic>
+#include <string>
+#include <thread>
+#include <chrono>
+#include <cstdint>
+#include <cstdlib>
+#include <unistd.h>
+
+#include <opencv2/video.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+
